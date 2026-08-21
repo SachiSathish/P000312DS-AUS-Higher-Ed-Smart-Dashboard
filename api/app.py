d09181c0ef2187becf82ac475135ceba87ca8ae1
Chloe Holmes <39513781+RattyGirl@users.noreply.github.com>
2026-08-21 17:11:53 +1000

    database setup

--- api/app.py
+++ api/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+DATABASE_URL = "postgresql://dashboard:dashboard@db:5432/dashboard"
+engine = create_engine(DATABASE_URL)
 
-
-@app.get("/")
+@app.get("/db_test")
 async def root():
+    with Session(engine) as session:
+        res = [str(row) for row in session.execute(sqlalchemy.text("SELECT version();")).all()]
+        print(", ".join(res))
+        return {"message": res}
+        
     return {"message": "Hello World"}
 
 
